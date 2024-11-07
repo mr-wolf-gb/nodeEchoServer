fe5f8efba3d1ec4504887e1a59a8d8bbf71b3167
Mr-Wolf-GB <gaith_baccari@hotmail.fr>
2024-11-07 09:41:54 +0100

    fix index access

--- echo.js
+++ echo.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 //app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); // Parse URL-encoded body
-app.use(express.static(path.join(__dirname))); // Serve files in the current directory
+app.use("/", express.static(path.join(__dirname, "public")));
 
 const server = http.createServer(app);
 const io = new Server(server, {
